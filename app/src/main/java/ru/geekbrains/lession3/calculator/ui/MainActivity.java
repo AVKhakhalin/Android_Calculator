7c6a546bf3388d5d624b0cfd8e5ca5b2227ee9b3
Khakhalin Andrey Vladimirovich <avkhakhalin@mail.ru>
2021-06-03 16:51:17 +0300

    Task 1.3. Buttons reassign actions without breakets.

--- app/src/main/java/ru/geekbrains/lession3/calculator/ui/MainActivity.java
+++ app/src/main/java/ru/geekbrains/lession3/calculator/ui/MainActivity.java
@@ -1,4 +1,4 @@
-package ru.geekbrains.lession3.calculator;
+package ru.geekbrains.lession3.calculator.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import ru.geekbrains.lession3.calculator.R;
 
 public class MainActivity extends AppCompatActivity
 {
